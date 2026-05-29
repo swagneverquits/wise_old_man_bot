de5c70f31f5d235f27b8fd408f49d36076d96320
swagneverquits <63224574+swagneverquits@users.noreply.github.com>
2026-05-28 23:32:37 -0400

    Fetch server data folder by default

--- deploy/fetch-data.ps1
+++ deploy/fetch-data.ps1
@@ -1,18 +1,28 @@
 param(
-    [string]$Name = "reply_audit.json",
+    [string]$Name = "",
     [string]$HostAlias = "oracle-reddit-bot",
-    [string]$RemoteRepo = "~/wise_old_man_bot",
+    [string]$RemoteRepo = "~/reddit_reply_bot",
     [string]$OutputName = ""
 )
 
 $ErrorActionPreference = "Stop"
 
-if ([System.IO.Path]::IsPathRooted($Name) -or $Name -match '(^|[\\/])\.\.([\\/]|$)') {
+if ($Name -and ([System.IO.Path]::IsPathRooted($Name) -or $Name -match '(^|[\\/])\.\.([\\/]|$)')) {
     throw "Name must be a path relative to the remote data directory."
 }
 
 $dataDir = Join-Path $PSScriptRoot "..\data"
 New-Item -ItemType Directory -Force -Path $dataDir | Out-Null
+
+if (-not $Name) {
+    $outputDir = Join-Path $dataDir "from-server"
+    New-Item -ItemType Directory -Force -Path $outputDir | Out-Null
+
+    $remotePath = "${HostAlias}:${RemoteRepo}/data/*"
+    scp -r $remotePath $outputDir
+    Write-Host "Fetched $remotePath -> $outputDir"
+    exit 0
+}
 
 if (-not $OutputName) {
     $baseName = [System.IO.Path]::GetFileNameWithoutExtension($Name)
